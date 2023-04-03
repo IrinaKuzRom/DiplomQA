c714e8f22789b9cef1e705f7df798f62b0623047
Irina <kuznetsova.ir.bog@gmail.com>
2023-04-03 20:58:53 +0300

    Initial commit

---
+++ src/test/java/ru/netology/web/entity/CreditRequestEntity.java
@@ -0,0 +1,12 @@
+package ru.netology.web.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CreditRequestEntity {
+    private String status;
+}
